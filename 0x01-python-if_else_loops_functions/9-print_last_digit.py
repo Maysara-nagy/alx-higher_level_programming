089976e20183e93b60fe6c479b5bfc7d8720c5a7
Maysara-nagy <maysara.nagy@gmail.com>
2023-01-18 21:16:55 +0000

    commited

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,9 +3,12 @@
 def print_last_digit(number):
     if number > 0:
         num = str(number)
+        print(num[-1])
         return num[-1]
     elif number < 0:
         num = str(number)
-        return -num[-1]
+        print(-int(num[-1]))
+        return -int(num[-1])
     else:
+        print("0")
         return 0
