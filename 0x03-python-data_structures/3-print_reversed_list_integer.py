ee0e077899accc9516261ba5544f8f4f7c6f4045
Maysara-nagy <maysara.nagy@gmail.com>
2023-02-27 02:36:33 +0000

    commited

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,8 +2,8 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.sort(reverse=True)
     if my_list is None:
         return
+    my_list.sort(reverse=True)
     for i in my_list:
         print("{:d}".format(i))
