892d4f4d62d4c408a12c0d11d5c947f8e33c36cc
Maysara-nagy <maysara.nagy@gmail.com>
2023-02-27 02:01:56 +0000

    commited

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,7 +4,7 @@ def element_at(my_list, idx):
     """my function"""
     if idx<0:
         return None
-    elif idx>len(my_list)):
+    elif idx>len(my_list) -1:
         return None
     else:
-        print("{}".format(my_list[idx]))
+        return my_list[idx]
