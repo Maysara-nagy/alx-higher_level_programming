664960f98e1e1ed6f77a2e3fd1267c78dcc230fb
Maysara-nagy <maysara.nagy@gmail.com>
2023-02-27 04:36:06 +0000

    commited

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,5 +3,5 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        my_list = my_list.sort()
-        return my_list[len(my_list) - 1]
+        my_list = my_list.sort(reverse = True)
+        return my_list[0]
