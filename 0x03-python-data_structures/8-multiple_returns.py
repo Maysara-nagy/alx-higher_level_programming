561111ce179294f152923653079f40d4e95014da
Maysara-nagy <maysara.nagy@gmail.com>
2023-02-27 04:21:01 +0000

    commited

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,10 +2,9 @@
 
 
 def multiple_returns(sentence):
+    my_tuple = tuple(sentence)
+    first = my_tuple[0]
+    len1 = len(my_tuple)
     if sentence is None:
         first = 0
-    else:
-        my_tuple = tuple(sentence)
-        first = my_tuple[0]
-        len1 = len(my_tuple)
-        return (len1, first)
+    return (len1, first)
