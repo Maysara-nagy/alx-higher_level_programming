41f47cc432e4d3dbf9e2c1561e991ce673d063d0
Maysara-nagy <maysara.nagy@gmail.com>
2023-03-06 22:29:37 +0200

    3. Present in both

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 # Write a function that returns a set of common elements in two sets.
 # Prototype: def common_elements(set_1, set_2):
 # You are not allowed to import any module
