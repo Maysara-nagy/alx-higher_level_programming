95d4f5a72c58c1c51f8be0006a799d1f4b56df94
Maysara-nagy <maysara.nagy@gmail.com>
2023-03-06 22:32:38 +0200

    4. Only differents

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Write a function that returns a set of all elements present in only one set.
+# Prototype: def only_diff_elements(set_1, set_2):
+# You are not allowed to import any module
+
+def only_diff_elements(set_1, set_2):
+    return (set_1-set_2)
