72ef93b5feca7cbef6b542a476548addcde9a6b2
Maysara-nagy <maysara.nagy@gmail.com>
2023-03-07 01:53:29 +0200

    0. Squared simple

--- 0-square_matrix_simple.py
+++ 0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-#Write a function that computes the square value of all integers of a matrix.
+# Write a function that computes the square value of all integers of a matrix.
 
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for i in matrix:
-        new_matrix.append(list(map(lambda i : i**2, i)))
+        new_matrix.append(list(map(lambda i: i**2, i)))
     return new_matrix
