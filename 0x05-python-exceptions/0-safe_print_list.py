758005dd810953e42267e89b8d5ac74fbeb5b625
Maysara-nagy <maysara.nagy@gmail.com>
2023-03-07 07:56:42 +0200

    0. Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,9 @@ def safe_print_list(my_list=[], x=0):
     sum = 0
     try:
         for i in range(x):
-            print(my_list[i], sep="")
+            print(my_list[i], end="")
             sum += 1
+        print()
         return sum
     except:
         return sum
