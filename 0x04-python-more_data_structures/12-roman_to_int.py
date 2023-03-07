ef69008186a848665ada4e7cd219744f3d4bc41d
Maysara-nagy <maysara.nagy@gmail.com>
2023-03-07 05:02:25 +0200

    12. Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# Technical interview preparation:
+
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) == str:
+        roman = list(roman_string)
+        sum = 0
+        for i in roman:
+            if i == 'I':
+                sum += 1
+            if i == 'V':
+                sum += 5
+            if i == 'X':
+                sum += 10
+            if i == 'L':
+                sum += 50
+            if i == 'D':
+                sum += 500
+            if i == 'M':
+                sum += 1000
+        return sum
+    else:
+        return 0
--- 0x04-python-more_data_structures/test.py
+++ 0x04-python-more_data_structures/test.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 # set items are not orderd and not indexed
 
-my_set = ["meso", "nagi", "100"]
-def function(my_set):
-    return True
-result = filter(function, my_set)
-for i in result:
-    print (result)
+roman = "meso"
+m = list(roman)
+print(m)
