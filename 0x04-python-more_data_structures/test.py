e27987f97d96944407879d24d5c4cda1064b4e8a
Maysara-nagy <maysara.nagy@gmail.com>
2023-03-07 02:59:35 +0200

    5. Number of keys

--- 0x04-python-more_data_structures/test.py
+++ 0x04-python-more_data_structures/test.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # set items are not orderd and not indexed
 
-my_set = {"meso", "nagi", "100"}
-my_set.clear()
-print(my_set)
+my_set = ["meso", "nagi", "100"]
+def function(my_set):
+    return True
+result = filter(function, my_set)
+for i in result:
+    print (result)
