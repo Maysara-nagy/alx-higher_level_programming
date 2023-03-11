da6d76438c2b470306c58b60e51de598acedad0c
Maysara-nagy <maysara.nagy@gmail.com>
2023-03-11 05:51:49 +0200

    4. Text indentation

---
+++ 0x07-python-test_driven_development/tests/5-main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+sys.path.insert(1, 'C:/Users/Maisara/Desktop/alx-higher_level_programming/0x07-python-test_driven_development')
+text_indentation = __import__('5-text_indentation').text_indentation
+
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
