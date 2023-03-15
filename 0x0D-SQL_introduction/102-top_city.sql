17bab37c67161c32522de98f4367d4200b5cb7f1
Maysara-nagy <maysara.nagy@gmail.com>
2023-03-15 07:44:39 +0200

    103-max_state.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- 102-top_city.sql
-SELECT city, MAX(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
