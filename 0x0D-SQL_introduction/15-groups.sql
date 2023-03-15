89ea871ad98451276b85d6b88ff2c88a85ff7bd3
Maysara-nagy <maysara.nagy@gmail.com>
2023-03-15 07:09:04 +0200

    15. Number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- 15. Number by score
-SELECT score, COUNT(*) FROM second_table GROUP BY score ORDER BY number DESC AS number;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC ;
