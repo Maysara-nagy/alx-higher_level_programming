4bcfd93105f95a5ef99b9f3a4f917a18577c0984
Maysara-nagy <maysara.nagy@gmail.com>
2023-03-16 03:46:45 +0200

    6. States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,14 @@
--- 6. States table
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa ;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.stats (
-    PRIMARY KEY (id),
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL
+-- -- 6. States table
+-- CREATE DATABASE IF NOT EXISTS hbtn_0d_usa ;
+-- CREATE TABLE IF NOT EXISTS hbtn_0d_usa.stats (
+--     PRIMARY KEY (id),
+--     id INT NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(256) NOT NULL
+-- );
+-- Creates the table hbtn_0d_usa with table states.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
 );
