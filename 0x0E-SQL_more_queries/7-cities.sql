7a3bd50a7ba1c4e65457e2e06d4d6387d5c9e887
Maysara-nagy <maysara.nagy@gmail.com>
2023-03-16 03:56:42 +0200

    7. Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,19 @@
--- 7. Cities table
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-    id INT AUT_INCREMENT NOT NULL ,
-    PRIMARY KEY (id),
-    state_id INT NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
-    name VARCHAR(256) NOT NULL
+-- -- 7. Cities table
+-- CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+--     id INT AUT_INCREMENT NOT NULL ,
+--     PRIMARY KEY (id),
+--     state_id INT NOT NULL,
+--     FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id),
+--     name VARCHAR(256) NOT NULL
+-- );
+-- Creates the database hbtn_0d_usa with the table cities.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+    PRIMARY KEY(`id`),
+    `id`       INT          NOT NULL AUTO_INCREMENT,
+    `state_id` INT          NOT NULL,
+    `name`     VARCHAR(256) NOT NULL,
+    FOREIGN KEY(`state_id`)
+    REFERENCES `hbtn_0d_usa`.`states`(`id`)
 );
