de4956ad7e77b4dc2a43861083be888ab8f01be2
Maysara-nagy <maysara.nagy@gmail.com>
2023-03-16 04:23:07 +0200

    cities.id - cities.name - states.name

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,3 +1,3 @@
 -- 9. Cities by States
 SELECT cities.id, cities.name, states.name FROM cities
-INNER JOIN states ON cities.id = states.id ORDER BY cities.id ;
+INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ;
