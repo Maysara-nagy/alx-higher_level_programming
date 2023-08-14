46a994faa133e7918cf70a62537392cf9beb0172
Maysara_Nagy <maysara.nagy@gmail.com>
2023-08-15 02:46:46 +0300

    7. I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Write a script that prints x times “C is fun”
 
-const x = Number(process.argv[2]);
+const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
