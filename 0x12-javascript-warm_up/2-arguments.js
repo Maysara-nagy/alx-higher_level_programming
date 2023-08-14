bf02423b63388f22a3ab3462bac47e5af3d435f3
Maysara_Nagy <maysara.nagy@gmail.com>
2023-08-15 02:13:48 +0300

    Arguments found

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,5 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 // Write a script that prints a message depending of the number of arguments passed:
 
 const args1 = process.argv.length;
-if (args1 === 2) {
-  console.log('No argument');
-} else if (args1 === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
+console.log(args1 === 2 ? 'No argument' : args1 === 3 ? 'Argument found' : 'Arguments found');
