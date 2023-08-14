00889867c9f822772874a38bcdd6d52e63389f7e
Maysara_Nagy <maysara.nagy@gmail.com>
2023-08-15 02:39:48 +0300

    JavaScript is amazing

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+// 6. Loop to languages
+
+const myString1 = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const x in myString1) {
+  console.log(myString1[x]);
+}
