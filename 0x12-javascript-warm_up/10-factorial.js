02a4d873edd429a9210b5c84cbc71c49649de8f2
Maysara_Nagy <maysara.nagy@gmail.com>
2023-08-15 03:09:36 +0300

    (x == 1)

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Write a script that computes and prints a factorial
+
+function fac (x) {
+    if (isNaN(x)) {
+      return 1
+    } if(x === 0){
+      return 1
+    } else{
+    return x * fac(x - 1)
+}}
+
+console.log(fac(Number(process.argv[2])));
