87891e45fddaba70419358fe5eeb8f9e3dfda64a
Maysara-nagy <maysara.nagy@gmail.com>
2024-07-05 19:47:06 +0300

    testing

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sI "$1" 0.0.0.0:5000/route_4 | grep -i "Allow" | cut -d " " -f 2-
+curl -sI "$1" | grep -i "Allow" | cut -d " " -f 2-
