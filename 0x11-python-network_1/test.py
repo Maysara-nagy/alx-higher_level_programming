6fbdf75db5b2aef4a16b7cbaaab421e6db3e4e95
Maysara-nagy <maysara.nagy@gmail.com>
2024-07-06 17:03:48 +0300

    adding 4-hbtn_status.py

--- 0x11-python-network_1/test.py
+++ 0x11-python-network_1/test.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-from urllib.request import *
-
-
-
-req = Request('http://www.python.org/fish.html')
-try:
-    urlopen(req)
-except HTTPError as e:
-    print(e.code)
-    print(e.read())  
+import requests
+response = requests.head("https://httpbin.org/get")
+print(response.history)
