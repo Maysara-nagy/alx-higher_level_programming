053a933deeaf8fc5b89408edac2635de20b3c68b
Maysara-nagy <maysara.nagy@gmail.com>
2024-07-06 17:37:38 +0300

    update it

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,6 +9,6 @@ import requests
 
 if __name__ == "__main__":
     url = argv[1]
-    email = {"email:":argv[2]}
+    email = {"email:": argv[2]}
     r = requests.post(url, data=email)
     print(r.txt)
