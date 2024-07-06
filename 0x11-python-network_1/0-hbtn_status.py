cfc3a5bfdd1c78d337b9bda9f864158a55b7cb88
Maysara-nagy <maysara.nagy@gmail.com>
2024-07-06 14:19:56 +0300

    update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''A Bash Script that fetches https://alx-intranet.hbtn.io/status.'''
+"""A Bash Script that fetches https://alx-intranet.hbtn.io/status."""
 from urllib.request import Request, urlopen
 
 if __name__ == "__main__":
