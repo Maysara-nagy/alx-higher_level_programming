0929a47648c5de79e7433e8b2d8afdbce7a1a7ea
Maysara-nagy <maysara.nagy@gmail.com>
2024-07-06 19:18:54 +0300

    adding last problem

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a Python script that takes 2 arguments
+    in order to solve this challenge."""
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    repoName = argv[1]
+    ownerName = argv[2]
+
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(repoName, ownerName))
+    for index in range(10):
+        print("{}: {}".format(
+            r.json()[index].get("sha"),
+            r.json()[index].get("commit").get("author").get("name")))
